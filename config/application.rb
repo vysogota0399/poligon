c6ee15a7f16b15e75a0e74328d29f6ab232416b8
Vysogota <you@example.com>
2024-06-14 15:10:04 +0300

    Интеграция с esia

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module Poligon
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
     config.eager_load_paths << Rails.root.join('lib')
+    config.time_zone = 'Moscow'
 
     # Configuration for the application, engines, and railties goes here.
     #
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,8 +1,12 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :ruesia,
     ENV['ESIA_ID'],
+    scope: 'openid fullname',
+    csp_server_url: 'http://csp_server:3037/cryptopro/sign',
+    cer_path: "#{Rails.root}/config/keys/esia.cer",
     client_options: {
-      site: 'https://esia-portal1.test.gosuslugi.ru/aas/oauth2/v2/ac',
-      cms_server_url: 'http://cryptopro/sign'
+      site: 'https://esia-portal1.test.gosuslugi.ru/',
+      authorize_url: 'aas/oauth2/v2/ac',
+      token_url: 'aas/oauth2/v2/te',
     }
 end
